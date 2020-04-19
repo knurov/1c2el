1ec78a22afc6498cc036312ce7ab48a7af3ecdca
knurov <knurov@mail.ru>
2020-04-19 12:46:47 +0300

    Добавил лог  и ротацию

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module knurov.ru/el/1c2el
 
 go 1.14
+
+require (
+	github.com/sirupsen/logrus v1.5.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -1,10 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"path/filepath"
+
+	// "log"
+	log "github.com/sirupsen/logrus"
+	lumberjack "gopkg.in/natefinch/lumberjack.v2"
+)
 
 // import args
 
 func main() {
-	// args.``
-	fmt.Println("Begin!")
+	var msg string
+	configName := flag.String("config", "./config.yaml", "Set config file path")
+	logpath := flag.String("log", "", "Set log path")
+	flag.StringVar(&msg, "message", "hello!", "just message")
+	flag.Parse()
+	fmt.Printf("Begin! %v\n", *configName)
+	fmt.Printf("%s end\n", msg)
+
+	if *logpath != "" {
+		logoutput := &lumberjack.Logger{
+			Filename: filepath.Join(*logpath, "1c2el.log"),
+			MaxSize:  100, // megabytes
+			// MaxBackups: 15,
+			MaxAge:   15,   //days
+			Compress: true, // disabled by default
+		}
+		log.SetOutput(logoutput)
+
+	}
+	log.SetLevel(log.TraceLevel)
+	log.Errorf("Err msq %v", msg)
+	log.Tracef("Trace val %v", log.TraceLevel)
+	log.Println("bue!")
 }
