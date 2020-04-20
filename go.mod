cd7ef410c83c510452e456ca53e6ffdbd120fb16
knurov <knurov@mail.ru>
2020-04-20 10:40:02 +0300

    Исправил чтение yaml

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/sirupsen/logrus v1.5.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/yaml.v2 v2.2.8
 )
--- main.go
+++ main.go
@@ -3,27 +3,49 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"path/filepath"
 
 	// "log"
 	log "github.com/sirupsen/logrus"
 	lumberjack "gopkg.in/natefinch/lumberjack.v2"
+	"gopkg.in/yaml.v2"
 )
-
-// import args
 
 func main() {
 	var msg string
-	configName := flag.String("config", "./config.yaml", "Set config file path")
-	logpath := flag.String("log", "", "Set log path")
+	configName := *(flag.String("config", "./config.yaml", "Set config file path"))
+	logpath := *(flag.String("log", "", "Set log path"))
 	flag.StringVar(&msg, "message", "hello!", "just message")
 	flag.Parse()
-	fmt.Printf("Begin! %v\n", *configName)
+	fmt.Printf("Begin! %v\n", configName)
 	fmt.Printf("%s end\n", msg)
 
-	if *logpath != "" {
+	type Config struct {
+		Database struct {
+			Host string `yaml:"host"`
+			Port int    `yaml:"port"`
+		} `yaml:"database"`
+	}
+
+	var config Config
+
+	if configName != "" {
+		configFile, confErr := ioutil.ReadFile(configName)
+		if confErr != nil {
+			log.Fatal(confErr)
+		}
+		yamlErr := yaml.Unmarshal(configFile, &config)
+		if yamlErr != nil {
+			log.Fatal(yamlErr)
+		}
+		fmt.Printf("Database.Host: %#v\n", config.Database.Host)
+
+	}
+
+	if logpath != "" {
 		logoutput := &lumberjack.Logger{
-			Filename: filepath.Join(*logpath, "1c2el.log"),
+			Filename: filepath.Join(logpath, "1c2el.log"),
 			MaxSize:  100, // megabytes
 			// MaxBackups: 15,
 			MaxAge:   15,   //days
